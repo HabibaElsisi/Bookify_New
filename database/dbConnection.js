2de8b03b03d40339f7f41299ffaca22833cd181b
HabibaElsisi <habibamsisi@gmail.com>
2024-04-04 00:56:41 +0200

    mongoAtlas

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,9 +1,12 @@
 import  mongoose  from "mongoose"
 
 export const dbConnection= ()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/BookifyNew")
+    mongoose.connect(`mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.49gv7u0.mongodb.net/Bookify`)
     .then(()=>console.log("mongo Database connected successfully"))
     .catch((err)=>{
         console.log("Database connected error",err)
     })
 }
+
+
+//mongodb://127.0.0.1:27017/BookifyNew
