a43d85ff83973578dbd63f9ecfcfddb5ff345e8f
HabibaElsisi <habibamsisi@gmail.com>
2024-04-23 06:14:46 +0200

    db_connection

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 import  mongoose  from "mongoose"
 
 export const dbConnection= ()=>{
-    mongoose.connect(`mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.49gv7u0.mongodb.net/Bookify`)
+    mongoose.connect(process.env.DB_ONLINE)
     .then(()=>console.log("mongo Database connected successfully"))
     .catch((err)=>{
         console.log("Database connected error",err)
@@ -10,3 +10,4 @@ export const dbConnection= ()=>{
 
 
 //mongodb://127.0.0.1:27017/BookifyNew
+
