d634decfb7abf9b8a88f227fbf27809a0378e369
HabibaElsisi <habibamsisi@gmail.com>
2024-05-01 16:24:46 +0300

    genre image val

--- src/modules/genre/genre.validation.js
+++ src/modules/genre/genre.validation.js
@@ -4,15 +4,15 @@ import joi from "joi"
 const addGenreVal=joi.object({
     name:joi.string().trim().required().min(2).max(100),
     image:joi.object({
-        fieldname:joi.string().required(),
-        originalname:joi.string().required(),
-        encoding:joi.string().required(),
-        mimetype:joi.string().valid('image/jpeg',"image/png","image/jpg","image/gif").required(),
-        size:joi.number().max(5242880).required(),
-        destination:joi.string().required(),
-        filename:joi.string().required(),
-        path:joi.string().required()
-    }).required(),
+        fieldname:joi.string(),
+        originalname:joi.string(),
+        encoding:joi.string(),
+        mimetype:joi.string().valid('image/jpeg',"image/png","image/jpg","image/gif"),
+        size:joi.number().max(5242880),
+        destination:joi.string(),
+        filename:joi.string(),
+        path:joi.string()
+    }),
     createdBy:joi.string().hex().length(24).optional()
 })
 const paramsIdVal=joi.object({
