949c8d70a90e1ce67010d9ba31e7a252905e6692
HabibaElsisi <habibamsisi@gmail.com>
2024-06-08 17:07:24 +0300

    ..

--- fast.js
+++ fast.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-const baseUrl = 'https://ab47-102-41-255-229.ngrok-free.app';
+const baseUrl = 'https://bda6-102-43-79-24.ngrok-free.app';
 
 const fetchBookRecommendations = async (bookName) => {
     const url = `${baseUrl}/recommend?book_name=${bookName}`;
